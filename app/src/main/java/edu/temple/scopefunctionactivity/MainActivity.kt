de00b53e63d40a2f9b303ca1598bb64cdbea3b4f
Elena <halvushka@gmail.com>
2025-10-01 16:52:13 -0400

    finished lab

--- app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import kotlin.random.Random
+import kotlin.random.nextInt
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,8 +20,29 @@ class MainActivity : AppCompatActivity() {
         // printing their output to the Log, which is visible in the LogCat:
         // eg. Log.d("function output", getTestDataArray().toString())
 
-    }
+        val testArray = getTestDataArray()
+        Log.d("testArray", testArray.toString())
 
+        Log.d("averageLessThanMedian", averageLessThanMedian(testArray as List<Double>).toString())
+
+        val root = findViewById<LinearLayout>(R.id.root)
+
+        val data = listOf(1,2,3,4)
+
+        var idx = 0
+        val v0 = getView(idx, null, data, this)
+        root.addView(v0)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+
+            idx +=1
+            if (idx >= data.size)
+            idx = 0
+            getView(idx, v0, data, this)
+
+        }
+
+    }
 
     /* Convert all the helper functions below to Single-Expression Functions using Scope Functions */
     // eg. private fun getTestDataArray() = ...
@@ -27,39 +51,70 @@ class MainActivity : AppCompatActivity() {
     // Look at the final/return value and build the function "working backwards"
 
     // Return a list of random, sorted integers
-    private fun getTestDataArray() : List<Int> {
-        val testArray = MutableList(10){ Random.nextInt()}
-        testArray.sort()
-        return testArray
-    }
+    //private fun getTestDataArray() : List<Int> {
+        //val testArray = MutableList(10){ Random.nextInt()}
+        //testArray.sort()
+        //return testArray
+    //}
+
+    private fun  getTestDataArray() : List<Int> = MutableList(10) { Random.nextInt() }.apply { sort() }
 
     // Return true if average value in list is greater than median value, false otherwise
-    private fun averageLessThanMedian(listOfNumbers: List<Double>): Boolean {
-        val avg = listOfNumbers.average()
-        val sortedList = listOfNumbers.sorted()
-        val median = if (sortedList.size % 2 == 0)
-            (sortedList[sortedList.size / 2] + sortedList[(sortedList.size - 1) / 2]) / 2
-        else
-            sortedList[sortedList.size / 2]
+    //private fun averageLessThanMedian(listOfNumbers: List<Double>): Boolean {
+        //val avg = listOfNumbers.average()
+        //val sortedList = listOfNumbers.sorted()
+        //val median = if (sortedList.size % 2 == 0)
+            //(sortedList[sortedList.size / 2] + sortedList[(sortedList.size - 1) / 2]) / 2
+        //else
+            //sortedList[sortedList.size / 2]
 
-        return avg < median
+        //return avg < median
+    //}
+
+    private fun averageLessThanMedian(listOfNumbers: List<Double>) : Boolean = listOfNumbers.sorted().let{
+
+        l ->
+
+            val median = if (l.size % 2 == 0)
+                (l[l.size / 2] + l[(l.size - 1) / 2]) / 2
+        else
+            l[l.size / 2 ]
+
+        listOfNumbers.average() < median
+
     }
+
+
 
     // Create a view from an item in a collection, but recycle if possible (similar to an AdapterView's adapter)
-    private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context): View {
-        val textView: TextView
+    //private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context): View {
+        //val textView: TextView
 
-        if (recycledView != null) {
-            textView = recycledView as TextView
-        } else {
-            textView = TextView(context)
-            textView.setPadding(5, 10, 10, 0)
-            textView.textSize = 22f
-        }
+        //if (recycledView != null) {
+            //textView = recycledView as TextView
+        //} else {
+            //textView = TextView(context)
+            //textView.setPadding(5, 10, 10, 0)
+            //textView.textSize = 22f
+        //}
 
-        textView.text = collection[position].toString()
+        //textView.text = collection[position].toString()
 
-        return textView
-    }
+        //return textView
+    //}
+
+    private fun getView(
+        position: Int,
+        recycledView: View?,
+        collection: List<Int>,
+        context: Context
+    ): View =
+
+        (recycledView as? TextView ?: TextView(context).apply {
+
+        setPadding(5, 10, 10, 0)
+        textSize = 22f
+    }).also {it.text = collection[position].toString()}
 
 }
+
